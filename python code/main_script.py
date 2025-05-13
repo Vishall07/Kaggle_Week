276aa91e09e11ef6ef1e07c2b92b0f4f509c475c
vishal <vgouli1998@gmail.com>
2025-05-13 12:16:23 +0200

    into functions

---
+++ python code/main_script.py
@@ -0,0 +1,91 @@
+import pandas as pd
+
+
+def read_file(path = ''):
+    with open(path, 'r') as file:
+        lines = file.read().strip().split('\n')
+    lines = lines[1:]
+    return lines
+
+
+def convert_lines_to_df(lines):
+    parsed = []
+    index = 0
+    for line in lines:
+        parts = line.strip().split()
+        painting_type = parts[0]
+        num_tags = int(parts[1])
+        tags = set(parts[2:]) 
+        parsed.append({
+            "index" : index,
+            "Type": painting_type,
+            "Num_Tags": num_tags,
+            "Tags": tags
+        })
+        index += 1
+    df = pd.DataFrame(parsed)
+    df = df.sort_values(by='Type', ascending=False)
+    return df
+
+def merge_potraits_in_one_frame(df):
+    df_p = df[df['Type'] == 'P'].copy()
+    df_l = df[df['Type'] == 'L'].copy()
+    merged = []
+    for i in range(0, len(df_p), 2):
+        if i + 1 < len(df_p):
+            idx1, idx2 = df_p.iloc[i]['index'], df_p.iloc[i+1]['index']
+            tags1, tags2 = df_p.iloc[i]['Tags'], df_p.iloc[i+1]['Tags']
+            tags3 = tags1.union(tags2)
+            merged.append({
+                "index": f"{idx1} {idx2}",
+                "Type" : "P",
+                "Tags": tags3,
+                "Num_Tags" : len(tags3)
+            })
+
+    merged_df = pd.DataFrame(merged)
+    combined_df = pd.concat([merged_df, df_l], ignore_index=True)
+    combined_df = combined_df.sort_values(by='Num_Tags', ascending=False)  
+    print(combined_df)
+    return combined_df
+
+
+def create_output_file(combined_df, path):
+    output = combined_df['index']  
+    len_output = len(output)
+
+    with open(path, "w") as f:
+        f.write(str(len_output) + "\n")
+        for line in output.values:
+            f.write(str(line)+'\n')
+
+def main(file_number = 0):
+    input_paths = [
+    "./Kaggle_Week/Data/0_example.txt",
+    "./Kaggle_Week/Data/1_binary_landscapes.txt",
+    "./Kaggle_Week/Data/10_computable_moments.txt",
+    "./Kaggle_Week/Data/11_randomizing_paintings.txt",
+    "./Kaggle_Week/Data/110_oily_portraits.txt"
+    ]
+
+    output_paths = [
+        "./Kaggle_Week/output/0_example.txt",
+        "./Kaggle_Week/output/1_binary_landscapes.txt",
+        "./Kaggle_Week/output/10_computable_moments.txt",
+        "./Kaggle_Week/output/11_randomizing_paintings.txt",
+        "./Kaggle_Week/output/110_oily_portraits.txt"
+    ]
+
+    
+    lines = read_file(input_paths[file_number])
+
+    df = convert_lines_to_df(lines)
+
+    combined_df = merge_potraits_in_one_frame(df)
+
+    create_output_file(combined_df, output_paths[file_number])
+
+
+if __name__=="__main__":
+    for i in range(5):
+        main(i)
---
+++ python code/test_score.py
@@ -0,0 +1,95 @@
+import pandas as pd
+
+
+def get_input_data(path = "../Data/0_example.txt"):
+    with open(path, 'r') as file:
+        lines = file.read().strip().split('\n')
+    lines = lines[1:]
+    return lines
+
+def get_output_data(path = "../output/0_example.txt"):
+    with open(path, 'r') as file:
+        lines = file.read().strip().split('\n')
+    lines = lines[1:]
+    return lines
+
+def input_lines_to_df(lines):
+    index = 0
+    parsed = []
+    for line in lines:
+        parts = line.strip().split()
+        painting_type = parts[0]
+        num_tags = int(parts[1])
+        tags = set(parts[2:]) 
+        parsed.append({
+            "index" : index,
+            "Type": painting_type,
+            "Num_Tags": num_tags,
+            "Tags": tags
+        })
+        index += 1
+    df = pd.DataFrame(parsed)
+    return df
+
+def output_lines_to_df(lines):
+    parsed = []
+    for line in lines:
+        parts = line.strip().split()
+        if(len(parts)==1):
+            parsed.append({"one" : parts[0], "two" : None})
+        else:
+            parsed.append({"one" : parts[0], "two" : parts[1]})
+    df = pd.DataFrame(parsed)
+    return df
+
+# scoring_function()
+
+input_paths = [
+    "../Data/0_example.txt",
+    "../Data/1_binary_landscapes.txt",
+    "../Data/10_computable_moments.txt",
+    "../Data/11_randomizing_paintings.txt",
+    "../Data/110_oily_portraits.txt"
+]
+
+output_paths = [
+    "../output/0_example.txt",
+    "../output/1_binary_landscapes.txt",
+    "../output/10_computable_moments.txt",
+    "../output/11_randomizing_paintings.txt",
+    "../output/110_oily_portraits.txt"
+]
+
+
+def get_score(input_path, output_path):
+    input_data = get_input_data(input_path)
+    output_data = get_output_data(output_path)
+    input_df = input_lines_to_df(input_data)
+    output_df = output_lines_to_df(output_data)
+
+    res = []
+    for i in range(len(output_df)):
+        first_paint = int(output_df.iloc[i]['one'])
+        set_one = input_df.iloc[first_paint]['Tags']
+        if(output_df.iloc[i]['two'] != None):
+            second_paint = int(output_df.iloc[i]['two'])
+            set_two = input_df.iloc[second_paint]['Tags']
+            set_one.union(set_two)
+        res.append({"tags" : set_one})
+    df = pd.DataFrame(res)
+
+
+    score = 0
+    for i in range(len(df)-1):
+        cur_set = df.iloc[i]['tags']
+        next_set = df.iloc[i+1]['tags']
+        score += min(len(cur_set & next_set), len(cur_set - next_set), len(next_set - cur_set))
+
+    print(score)
+
+
+
+
+if __name__=="__main__":
+    for i in range(len(input_paths)):
+        get_score(input_paths[i], output_paths[i])
